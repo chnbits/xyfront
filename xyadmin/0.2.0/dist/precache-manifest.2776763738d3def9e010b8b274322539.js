4882b8c42c0b5c15b2dac72e27d4de731ab9e4d7
jry <ijry@qq.com>
2020-08-11 13:45:19 +0800

    auto commit

---
+++ xyadmin/0.2.0/dist/precache-manifest.2776763738d3def9e010b8b274322539.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865fa318800289254795b60c12d2acdb",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist//admin/manifest.json"
+  },
+  {
+    "revision": "5c2d54c4bd1a8561fbae2d2e216d7e7b",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/audio/alert.wav"
+  },
+  {
+    "revision": "3087cb497c9b72b10c00969b007af481",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/audio/close.wav"
+  },
+  {
+    "revision": "da29995693b4090fca6b32edc1fb3ce0",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/audio/delete.wav"
+  },
+  {
+    "revision": "700a76157bdaed31213ebefd9ba6ca4d",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/audio/open.wav"
+  },
+  {
+    "revision": "36b22bc5d424e4e7c839260c5d34cc5e",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/audio/tab.wav"
+  },
+  {
+    "revision": "4c350441bf237e26a074",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/css/app.d197ac08.css"
+  },
+  {
+    "revision": "f3e0f36dbe51a314a465",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/css/manifest.2fdb4fec.css"
+  },
+  {
+    "revision": "32b9f3ea8145a30a0f08",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/css/vendor.77d10ac0.css"
+  },
+  {
+    "revision": "068ca2b316db98037bebdd1e4f1b9459",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/fonts/fontello.068ca2b3.ttf"
+  },
+  {
+    "revision": "8d4a4e6f7431d0d7fa92b1df20f38161",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/fonts/fontello.8d4a4e6f.woff2"
+  },
+  {
+    "revision": "a782baa8633b1359f9686ffad17e0d76",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/fonts/fontello.a782baa8.woff"
+  },
+  {
+    "revision": "e73a0647198cfe970de0f003be95cc51",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/fonts/fontello.e73a0647.eot"
+  },
+  {
+    "revision": "ef222224f2903952bfa54c614cbb0c80",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/img/app.ef222224.png"
+  },
+  {
+    "revision": "dae4ad504d68eb2fd96ab711d66c9874",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/img/color.dae4ad50.png"
+  },
+  {
+    "revision": "9354499c2824248511adf85fdf8e4c37",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/img/fontello.9354499c.svg"
+  },
+  {
+    "revision": "6caba44d1c88a86fe1973d83cb9f0d28",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/img/iconfont.6caba44d.svg"
+  },
+  {
+    "revision": "a49e406dfd0ab01803d1edbd8d2c4100",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/img/qun_qq.a49e406d.png"
+  },
+  {
+    "revision": "19704c36816578585bc8e9087af427ff",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/index.html"
+  },
+  {
+    "revision": "4c350441bf237e26a074",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/js/app.427377f2.js"
+  },
+  {
+    "revision": "f3e0f36dbe51a314a465",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/js/manifest.71fe74ab.js"
+  },
+  {
+    "revision": "32b9f3ea8145a30a0f08",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/js/vendor.096fc056.js"
+  },
+  {
+    "revision": "61e84baf6e0623e3e0a4106612ce67f3",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/langs/zh_CN.js"
+  },
+  {
+    "revision": "21d51aa3b251ee666fe63f41473e4731",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/content/dark/content.css"
+  },
+  {
+    "revision": "ca8f0c08415563689b59d52cc029815b",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/content/dark/content.min.css"
+  },
+  {
+    "revision": "7731675a9091b95d8882d88fd44ea978",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/content/default/content.css"
+  },
+  {
+    "revision": "f92b7b2906b654e2dfb3650fa44a9b42",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/content/default/content.min.css"
+  },
+  {
+    "revision": "4dc634a4f2def9e0f376a93589928ada",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/content/document/content.css"
+  },
+  {
+    "revision": "91f332a1c4884ad5adf4107e304f84fe",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/content/document/content.min.css"
+  },
+  {
+    "revision": "e0b194a072920c39e1e3818b74896063",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/content/writer/content.css"
+  },
+  {
+    "revision": "8cd71063696a2f374dd4e479c2cdcb41",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/content/writer/content.min.css"
+  },
+  {
+    "revision": "4001cfb83fd6d7116264f5f4f425fc6f",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide-dark/content.css"
+  },
+  {
+    "revision": "35170ab0498861f4fae7a07b2bb84581",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide-dark/content.inline.css"
+  },
+  {
+    "revision": "bdb6f93df5c249b76ef50e18caebf40f",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide-dark/content.inline.min.css"
+  },
+  {
+    "revision": "0a88ced66dd018baf265f065db26194d",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide-dark/content.min.css"
+  },
+  {
+    "revision": "e84062c0d71bace6637586be7c2590d3",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide-dark/content.mobile.css"
+  },
+  {
+    "revision": "4e83ff7c5a3009d223b4502e51e6428e",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide-dark/content.mobile.min.css"
+  },
+  {
+    "revision": "baecf466c40e709e7ffdbc935fc0813a",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide-dark/fonts/tinymce-mobile.woff"
+  },
+  {
+    "revision": "2707676c374ad9cac21dce9e5809e568",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide-dark/skin.css"
+  },
+  {
+    "revision": "305dbb80393f546646a134763ffe5cdc",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide-dark/skin.min.css"
+  },
+  {
+    "revision": "facca67ad6951942195189b0478e4d4b",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide-dark/skin.mobile.css"
+  },
+  {
+    "revision": "5d4667e1b66bb2bda0c4a06e6d8a0878",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide-dark/skin.mobile.min.css"
+  },
+  {
+    "revision": "58aa1f4cbeed1b2df36a4da55295711e",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide/content.css"
+  },
+  {
+    "revision": "35170ab0498861f4fae7a07b2bb84581",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide/content.inline.css"
+  },
+  {
+    "revision": "bdb6f93df5c249b76ef50e18caebf40f",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide/content.inline.min.css"
+  },
+  {
+    "revision": "8aae8fca7388a0c238656a873dca74c3",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide/content.min.css"
+  },
+  {
+    "revision": "e84062c0d71bace6637586be7c2590d3",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide/content.mobile.css"
+  },
+  {
+    "revision": "4e83ff7c5a3009d223b4502e51e6428e",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide/content.mobile.min.css"
+  },
+  {
+    "revision": "baecf466c40e709e7ffdbc935fc0813a",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide/fonts/tinymce-mobile.woff"
+  },
+  {
+    "revision": "ce81f08db86065b77480fcea02e4c516",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide/skin.css"
+  },
+  {
+    "revision": "6d48911535d0b7cd799e8248c696fb46",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide/skin.min.css"
+  },
+  {
+    "revision": "facca67ad6951942195189b0478e4d4b",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide/skin.mobile.css"
+  },
+  {
+    "revision": "5d4667e1b66bb2bda0c4a06e6d8a0878",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/libs/tinymce/skins/ui/oxide/skin.mobile.min.css"
+  },
+  {
+    "revision": "eb0e6ac50f334d95e0e524dd0a81dd4b",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/logo.png"
+  },
+  {
+    "revision": "12422e6e33c078a83810c7397755a6bc",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/noimage.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/robots.txt"
+  }
+]);
--- xyadmin/0.2.0/dist/reg-sw.js
+++ xyadmin/0.2.0/dist/reg-sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.28cda45317c8d955f785241d8e61c3ca.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://cdn.jsdelivr.net/gh/starideas/xyfront/xyadmin/0.2.0/dist/precache-manifest.2776763738d3def9e010b8b274322539.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-console */
 
