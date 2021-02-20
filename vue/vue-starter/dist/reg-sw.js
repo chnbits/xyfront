bf5c6d5a4534ddf32f1d7df784bcb645344395b7
jry <ijry@qq.com>
2021-02-20 21:23:55 +0800

    auto commit

--- vue/vue-starter/dist/reg-sw.js
+++ vue/vue-starter/dist/reg-sw.js
@@ -1,4 +1,4 @@
-importScripts("https://cdn.jsdelivr.net/gh/starideas/xyfront/vue/vue-starter/dist/precache-manifest.b21818affa7a29be650a7ff23b34d1b9.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://cdn.jsdelivr.net/gh/starideas/xyfront/vue/vue-starter/dist/precache-manifest.5c0c3d54e10393d9218fc475c81e4836.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-console */
 
