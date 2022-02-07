dc401f98b96607f106b4da4a9ff5a940679f8302
jry <ijry@qq.com>
2022-02-07 14:53:02 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,11 @@
-cli hosting deploy --provider aliyun --space xycloud --source ./
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+if [ -n "$1" ]; then
+    cli hosting deploy --provider aliyun --space xycloud --source ./$1 --prefix /$1
+else
+    cli hosting deploy --provider aliyun --space xycloud --source ./
+fi
+
