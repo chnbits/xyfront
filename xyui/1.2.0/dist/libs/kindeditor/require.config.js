2af9282a7d941110e5232d61fc3cc87c91bd2e21
jry <ijry@qq.com>
2022-04-07 18:44:19 +0800

    auto commit

---
+++ xyui/1.2.0/dist/libs/kindeditor/require.config.js
@@ -0,0 +1,27 @@
+require.config({
+    paths: {
+        'nkeditor': '../addons/nkeditor/js/customplugin',
+        'nkeditor-core': '../addons/nkeditor/nkeditor',
+        'nkeditor-lang': '../addons/nkeditor/lang/zh-CN',
+    },
+    shim: {
+        'nkeditor': {
+            deps: [
+                'nkeditor-core',
+                'nkeditor-lang'
+            ]
+        },
+        'nkeditor-core': {
+            deps: [
+                'css!../addons/nkeditor/themes/black/editor.min.css',
+                'css!../addons/nkeditor/css/common.css'
+            ],
+            exports: 'window.KindEditor'
+        },
+        'nkeditor-lang': {
+            deps: [
+                'nkeditor-core'
+            ]
+        }
+    }
+});
