b2a018d2a59191c2e8eb233e95cde560bfa224e4
jry <ijry@qq.com>
2022-06-17 21:47:41 +0800

    auto commit

---
+++ vue/vue-no7/dist/precache-manifest.30964968211cd3201ba17f6af552bdc5.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e0f419867632c90b07",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/css/app.c97d91cd.css"
+  },
+  {
+    "revision": "a0d0d19ad94ccd150332",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/css/manifest.df69e6ac.css"
+  },
+  {
+    "revision": "9c41971b07e557d885d6",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/css/vendor.ba2bebcb.css"
+  },
+  {
+    "revision": "46ac6629168546c71dbf115992aa4be9",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/fonts/VideoJS.46ac6629.eot"
+  },
+  {
+    "revision": "c3b4dd8a8cda25a35d922c17beaca1e3",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/al1.c3b4dd8a.png"
+  },
+  {
+    "revision": "dda7089d09f4eb28c65a066a6c944ace",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/al2.dda7089d.png"
+  },
+  {
+    "revision": "23bd5e7f45f0f8c0640657886fce8ad1",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/al3.23bd5e7f.png"
+  },
+  {
+    "revision": "97144cdc0c8537690b335a0bff421be9",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/banner.97144cdc.png"
+  },
+  {
+    "revision": "8a8300195e4962c3690718d846a6e0a3",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/block1-1.8a830019.png"
+  },
+  {
+    "revision": "cb91a8d5cb525fca1b3fffbf42ff7807",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/block1-2.cb91a8d5.png"
+  },
+  {
+    "revision": "7c9c8c02df5948897fde20926e84af0b",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/block1-3.7c9c8c02.png"
+  },
+  {
+    "revision": "ee917cc7dba5fc78b771f5b6ac5f5c3f",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/block1-4.ee917cc7.png"
+  },
+  {
+    "revision": "7f0698455a9c2c0f208701647ccf6023",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/block2.7f069845.png"
+  },
+  {
+    "revision": "677b4d61d817ab823e86cfaff802b2bc",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/fa1.677b4d61.png"
+  },
+  {
+    "revision": "8271ba0a332b8eb80836d067136b9ecc",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/fa2.8271ba0a.png"
+  },
+  {
+    "revision": "c85bea3de4c5b70079aa4e97570c3cc4",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/fa3.c85bea3d.png"
+  },
+  {
+    "revision": "77571e8b89325e6dd7331b0e740eb959",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/fa4.77571e8b.png"
+  },
+  {
+    "revision": "01afa3743ce0515909b7f6427843a45a",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/renyuan.01afa374.png"
+  },
+  {
+    "revision": "0dd19d16ee604ab0242aa3da9ecc3542",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/youshi.0dd19d16.png"
+  },
+  {
+    "revision": "0ec5ee17172ae2d402b6121b3e6fdde5",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/ys1.0ec5ee17.png"
+  },
+  {
+    "revision": "4fa5453522fc7104afe165d0aa7fb061",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/ys2.4fa54535.png"
+  },
+  {
+    "revision": "a5c9ac7e50dd9e74e86a1855e3b894ae",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/ys3.a5c9ac7e.png"
+  },
+  {
+    "revision": "a22f3051aafb7cf81291217ddadee79a",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/img/ys4.a22f3051.png"
+  },
+  {
+    "revision": "6619bfc776f708a45f4d48e3a59f8474",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/index.html"
+  },
+  {
+    "revision": "85e0f419867632c90b07",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/js/app.cfcaa03e.js"
+  },
+  {
+    "revision": "a0d0d19ad94ccd150332",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/js/manifest.b5b4b039.js"
+  },
+  {
+    "revision": "9c41971b07e557d885d6",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/js/vendor.af387177.js"
+  },
+  {
+    "revision": "eb0e6ac50f334d95e0e524dd0a81dd4b",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/logo.png"
+  },
+  {
+    "revision": "49172f71afc6ed8c49ff7fcf0bed7225",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://cdn.jiangruyi.com/vue/vue-no7/dist/robots.txt"
+  }
+]);
