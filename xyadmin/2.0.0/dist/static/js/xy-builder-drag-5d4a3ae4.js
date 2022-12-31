e6e98ee604f48ed296e74ab02ce702a9e9533c01
jry <ijry@qq.com>
2023-01-01 00:04:02 +0800

    auto commit

---
+++ xyadmin/2.0.0/dist/static/js/xy-builder-drag-5d4a3ae4.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-1b428a4d.js";const o={},t=window.Vue.openBlock,r=window.Vue.createElementBlock;const n=e(o,[["render",function(e,o){return t(),r("div")}]]),l=Object.freeze(Object.defineProperty({__proto__:null,default:n},Symbol.toStringTag,{value:"Module"}));export{n as X,l as _};
